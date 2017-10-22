676514e1855c459be3266d2790d08420f5a75cc9
Smythe <dandycatdesigns@gmail.com>
2017-10-22 09:52:34 -0500

    Removed dupe JS file.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.getElementById('change').onload = changeBackground();
 
 function changeBackground() {
-	let arr = ['img0.jpg', 'img1.jpg', 'img2.jpg', 'img3.jpg'];
+	let arr = ['img0.jpg', 'img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg'];
 	document.body.style.background = "url('img/" + arr[Math.floor((Math.random() * 3))] + "')";
 }
--- main.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('change').onload = changeBackground();
-
-function changeBackground() {
-	const arr = ['img0.jpg', 'img1.jpg', 'img2.jpg', 'img3.jpg'];
-	document.body.style.background = "url('img/" + arr[Math.floor((Math.random() * 3))] + "')";
-}
