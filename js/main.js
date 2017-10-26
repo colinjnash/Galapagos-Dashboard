cb3e35ba993499d721ae4dc2dfab8fb043098f38
Colin Nash <colinjnash@gmail.com>
2017-10-26 13:24:54 -0400

    Increaded to-do list functionality

--- js/main.js
+++ js/main.js
@@ -13,97 +13,120 @@
 /* Onload Scripts
  *********************************************/
 $(document).ready(function($) {
+	$('#welcome').hide().fadeIn('slow');
+	$(document).ready(function() {
+		$('#welcome').removeClass('hidden');
+	});
 
-    $('#welcome').hide().fadeIn('slow');
-    $(document).ready(function() {
-        $('#welcome').removeClass('hidden');
-    });
 
-
-    changeBackground();
-    buildClock();
+	changeBackground();
+	buildClock();
 
    
-    
+	
 
-    function changeBackground() {
-        let arr = ['img0.jpg', 'img1.jpg', 'img2.jpg', 'img3.jpg'];
-        document.body.style.background = "url('img/" + arr[Math.floor((Math.random() * 3))] + "')";
-    }
+	function changeBackground() {
+		var arr = ['img0.jpg', 'img1.jpg', 'img2.jpg', 'img3.jpg'];
+		document.body.style.background = "url('img/" + arr[Math.floor((Math.random() * 3))] + "')";
+	}
 
-    // Fade in the div
-
-
+	// Fade in the div
 
 
 
 
 
-    /* Nav Menu
-     *********************************************/
-    // Two functions to open and close side menu
-    document.getElementById('openMenu').onclick = function() {
-        document.getElementById('leftNav').style.width = '250px'
-    };
-    document.getElementById('closeMenu').onclick = function() {
-        document.getElementById('leftNav').style.width = '0'
-    };
-
-    /* Clock
-     *********************************************/
-    // Web clock on the front page
-    function buildClock() {
-        let clock = new Date();
-        let h = clock.getHours();
-        let m = clock.getMinutes();
-        // let s = clock.getSeconds();
-
-        m = addZero(m);
-        // s = addZero(s);		
-
-        let t = setTimeout(buildClock, 500);
-        let dayNight = ' AM';
-
-        if (h > 12) {
-            dayNight = ' PM';
-            h -= 12;
-        } else if (h = 12) {
-            dayNight = ' PM';
-        } else if (h = 0) {
-            dayNight = ' AM';
-        }
-
-        document.getElementById('clock').innerHTML = h + ':' + m + dayNight;
-    }
-
-    function addZero(i) {
-        if (i < 10) {
-            return '0' + i;
-        }
-        return i;
-    }
 
 
+	/* Nav Menu
+	 *********************************************/
+	// Two functions to open and close side menu
+	document.getElementById('openMenu').onclick = function() {
+		document.getElementById('leftNav').style.width = '250px';
+	};
+	document.getElementById('closeMenu').onclick = function() {
+		document.getElementById('leftNav').style.width = '0';
+	};
 
-    /* To Do List
-     *********************************************/
-    // Loads data from input to list on right side bar
-    $('input#toDoItem').keypress(function(e) {
+	/* Clock
+	 *********************************************/
+	// Web clock on the front page
+	function buildClock() {
+		let clock = new Date();
+		let h = clock.getHours();
+		let m = clock.getMinutes();
+		// let s = clock.getSeconds();
 
-        if (e.which === 13) {
-        	console.log("ENTER");
-            let toDoText = $(this).val();
+		m = addZero(m);
+		// s = addZero(s);		
+
+		// let t = setTimeout(buildClock, 500);
+		let dayNight = ' AM';
+
+		if (h > 12) {
+			dayNight = ' PM';
+			h -= 12;
+		} else if (h == 12) {
+			dayNight = ' PM';
+		} else if (h == 0) {
+			dayNight = ' AM';
+		}
+
+		document.getElementById('clock').innerHTML = h + ':' + m + dayNight;
+	}
+
+	function addZero(i) {
+		if (i < 10) {
+			return '0' + i;
+		}
+		return i;
+	}
 
 
-            $('.rightNav').append('<li>' + toDoText + '</li>');
 
-            // Clear out the input field
-            $(this).val('');
-            e.preventDefault();
-        }
-    });
+	/* To Do List
+	 *********************************************/
+	// Loads data from input to list on right side bar
+	$('input#toDoItem').keypress(function(e) {
 
-    /* Setting Up ToDoToggle
-     *****************************************************************/
+		if (e.which === 13) {
+			
+			let toDoText = $(this).val();
 
+
+			$('#toggleList').append('<li class="todo">' + toDoText + '<span class="delete">' + ' ' + '<i class="fa fa-trash"></i></span>' + '</li>');
+
+			// Clear out the input field
+			$(this).val('');
+			e.preventDefault();
+		}
+
+	});
+
+	$('span').on('click', function(){
+
+	});
+
+	// ToDoToggle Functionality
+	 // *****************************************************************/
+	$(document).on('click', '.delete', function(event) {
+		$(this).parent().fadeOut(300,function(){
+			$(this).remove();
+		});
+		event.stopPropagation();
+	});
 });
+
+
+	// ToDoDiv Toggle
+
+	$('#toDoTitle').on('click', function(){
+		$('#toggleList').toggleClass("hidden");
+	});
+
+
+
+
+
+
+
