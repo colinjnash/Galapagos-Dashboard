58f87c38a9bfcd0fc69c0e55b217a9b45615e8c7
Colin Nash <colinjnash@gmail.com>
2017-11-04 18:44:35 -0400

    Firebase integrated with chrome extension

--- js/firebase.js
+++ js/firebase.js
@@ -56,23 +56,9 @@ const addTodo = (todo, user, timeStamp) => {
 	todos.innerHTML += '<li class="todoli" >' + '<input type="checkbox">' + `${todo} - ${user}` + '<span class="delete">' + ' ' + '<i class="fa fa-trash"></i></span>' + '</li>';
 };
 
-var provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
+chrome.identity.getAuthToken({}, function(token) {
+ var credential = firebase.auth.GoogleAuthProvider.credential(null, token);
+ firebase.auth().signInWithCredential(credential);
 });
-
 
 
