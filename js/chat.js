5feb41b8eea41da3430b0daf3c3bbbc26b0684b8
jascottt7 <jayscott1@gmail.com>
2017-11-10 20:04:28 +0000

    fixed username issue

--- js/chat.js
+++ js/chat.js
@@ -1,13 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
   var messagesList = document.getElementById('messages'),
       textInput = document.getElementById('text'),
-      sendButton = document.getElementById('send'),
-      username = "Jason";
-  
-  var app = firebase.initializeApp(config),
-      database = app.database();
+      sendButton = document.getElementById('send');
+	  var username = '';
+	chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+	
+	// Use the token.
+		var provider = new firebase.auth.GoogleAuthProvider().credential(null,token);
+		// var credential = firebase.auth.GoogleAuthProvider.credential(null, token);
+		firebase.auth().signInWithCredential(provider).then(function(result){
+			var userId = firebase.auth().currentUser.uid;
+			return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+				username = snapshot.val().Name;	
+				console.log(username);
+
+			});
+		});
+	});
       
-  var databaseRef = database.ref().child('chat');
+  var databaseRef = firebase.database().ref("chat/");
   sendButton.addEventListener('click', function(evt) {
     var chat = { name: username, message: textInput.value };
     databaseRef.push().set(chat);
@@ -25,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function() {
   
   function addMessage(chat) {
     let chatItem = document.getElementById("messages");
-    chatItem.innerHTML += '<li><div class="chatBubble reply"><img src="//www.gravatar.com/avatar/0?d=mm&s=30" class="avatar"/><small>'+ chat.name + ' says,</small><br>' + chat.message + '</div></li>';
+    chatItem.innerHTML += '<li><div class="chatBubble reply"><small>'+ chat.name + ' says,</small><br>' + chat.message + '</div></li>';
     scrollToBottom("messages");
   }
 });
